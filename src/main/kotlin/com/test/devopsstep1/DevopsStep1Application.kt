323b62a644212d2c952b3e126af4cd77a55245a9
Mohamed TOURE <toure1206@gmail.com>
2020-04-18 13:20:37 +0000

    First commit

---
+++ src/main/kotlin/com/test/devopsstep1/DevopsStep1Application.kt
@@ -0,0 +1,11 @@
+package com.test.devopsstep1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevopsStep1Application
+
+fun main(args: Array<String>) {
+	runApplication<DevopsStep1Application>(*args)
+}
---
+++ src/test/kotlin/com/test/devopsstep1/DevopsStep1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.devopsstep1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevopsStep1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
